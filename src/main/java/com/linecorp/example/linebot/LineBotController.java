76f399dddd27cb711e92ba09fb2b7d836b1d6c06
LINE <line@LINE_ID_8cb6d3.local>
2016-12-15 15:52:52 +0700

    debugging

--- src/main/java/com/linecorp/example/linebot/LineBotController.java
+++ src/main/java/com/linecorp/example/linebot/LineBotController.java
@@ -111,8 +111,8 @@ public class LineBotController
             System.out.println("Payload: " + aPayload);
         }
         
-        Gson gson = new Gson();
-        Payload[] payload = gson.fromJson(aPayload, Payload[].class);
+//        Gson gson = new Gson();
+//        Payload[] payload = gson.fromJson(aPayload, Payload[].class);
         
         //Parsing JSONObject from source
         JSONObject jObject = new JSONObject(aPayload);
